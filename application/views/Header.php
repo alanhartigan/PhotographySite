4f2c17e0c560b7e83b93451ce193c0f9b52c3e44
alanhartigan <alan.hartigan15@live.com>
2018-05-16 21:38:03 +0100

    Fix header one last time

--- application/views/Header.php
+++ application/views/Header.php
@@ -1,5 +1,4 @@
 <?php>
-<<<<<<< HEAD
   <link rel="stylesheet" href="CSS/main.css" type="text/css">
   <head>
     <div class="header">
@@ -11,16 +10,3 @@
       </div>
     </div>
   </head>
-=======
-<link rel="stylesheet" type="text/css" href="..\css\main.css">.
-<head>
-<div class="header">
-  <a href="#default" class="logo">CompanyLogo</a>
-  <div class="header-right">
-    <a class="active" href="#home">Home</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-  </div>
-</div>
-</head>
->>>>>>> 622690292c368cc542e6423c4482a592b7591fbc
