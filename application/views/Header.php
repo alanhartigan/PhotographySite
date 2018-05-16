581bba5485644ad401a5ce405858b2bca4af2dd2
alanhartigan <alan.hartigan15@live.com>
2018-05-16 21:09:31 +0100

    Header

---
+++ application/views/Header.php
@@ -0,0 +1,12 @@
+<?php>
+<link rel="stylesheet" type="text/css" href="E:\Documents\VS Code Projects\PhotographySite\CSS\CSS Main">.
+<head>
+<div class="header">
+  <a href="#default" class="logo">CompanyLogo</a>
+  <div class="header-right">
+    <a class="active" href="#home">Home</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+  </div>
+</div>
+</head>
