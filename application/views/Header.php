8b195e8e0b912e2475af0611f1b6aadb7d55ce28
alanhartigan <alan.hartigan15@live.com>
2018-05-16 21:19:25 +0100

    CSS working

--- application/views/Header.php
+++ application/views/Header.php
@@ -1,12 +1,14 @@
 <?php>
-<link rel="stylesheet" type="text/css" href="..\CSS\CSS Main">.
-<head>
-<div class="header">
-  <a href="#default" class="logo">CompanyLogo</a>
-  <div class="header-right">
-    <a class="active" href="#home">Home</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-  </div>
-</div>
-</head>
+  <style>
+    <?php include 'CSS/main.css'; ?>
+  </style>
+  <head>
+    <div class="header">
+      <a href="#default" class="logo">CompanyLogo</a>
+      <div class="header-right">
+        <a class="active" href="#home">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+    </div>
+  </head>
