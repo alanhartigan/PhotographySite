9470fec23a96f9162f4f5c9730d7575bf1dfdf28
bulbu1 <30908842+bulbu1@users.noreply.github.com>
2018-05-16 21:44:57 +0100

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,11 @@
     </body>
     <footer>
-        <a href="#default" class="logo">CompanyLogo</a>
+        <div class="footer">
+            <a href="#default" class="logo">CompanyLogo</a>
+            <div class="footer-right">
+            <a class="active" href="#home">Home</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
     </footer> 
 </html>
